abc6233068cd9b59f50a321bfa96b071dd5df579
Javicadserres <javier.cardenas.serres@gmail.com>
2020-09-09 01:04:26 +0200

    CLN: fixing spelling mistakes

--- kelly/part_1_continuous_variables/utils.py
+++ kelly/part_1_continuous_variables/utils.py
@@ -80,3 +80,38 @@ def get_cumulative_returns(returns):
     cum_returns = cum_returns.dropna()
     
     return cum_returns
+
+def backtest(kelly_df, returns_df, leverages):
+    """Backtests Kelly strategy
+    
+    Parameters
+    ----------
+    kelly_df : pd.DataFrame
+        kelly optimal allocations for the securities
+    returns_df : pd.DataFrame
+        daily returns of the securities
+    leverages : list
+        list containing the number of leverages to study
+        
+    Returns
+    -------
+    total_returns : pd.DataFrame
+    """
+    
+    total_returns = pd.DataFrame()
+
+    for leverage in leverages:
+        kelly_weights = kelly_df.copy()
+
+        # restrict shortselling
+        kelly_weights[kelly_weights<0] = 0
+
+        daily_weights_sum = kelly_weights.sum(axis=1)
+        leverage_cond = daily_weights_sum > leverage
+
+        kelly_weights[leverage_cond] = leverage * kelly_weights[leverage_cond].div(daily_weights_sum[leverage_cond], axis=0)
+
+        name = 'max_leverage_' + str(leverage) 
+        total_returns[name] = (returns_df * kelly_weights).sum(axis=1)
+        
+    return total_returns
